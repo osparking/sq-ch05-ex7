343ef41e3a8e0a4a93ff773877414e8fdaa6639f
osparking <jbpark03@gmail.com>
2023-09-09 23:20:28 +0900

    comment class and processor that changes it

---
+++ src/main/java/service/CommentProcessor.java
@@ -0,0 +1,23 @@
+package service;
+
+import domain.Comment;
+
+public class CommentProcessor {
+	private Comment comment;
+
+	public Comment getComment() {
+		return comment;
+	}
+
+	public void setComment(Comment comment) {
+		this.comment = comment;
+	}
+	
+	public void processComment() {
+		// change comment
+	}
+	
+	public void validateComment() {
+		// change comment and validate comment
+	}
+}
