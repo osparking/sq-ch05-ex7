f2e0fdc8abd35868fb5e08fca440f46951499b80
osparking <jbpark03@gmail.com>
2023-09-10 10:26:43 +0900

    prototype bean read inside of method

--- src/main/java/service/CommentService.java
+++ src/main/java/service/CommentService.java
@@ -1,5 +1,7 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import domain.Comment;
@@ -7,14 +9,19 @@ import domain.Comment;
 @Service
 public class CommentService {
 	
+	@Autowired
+	private ApplicationContext context;
+	
 	public void sendComment(Comment c) {
-		var p = new CommentProcessor();
+		
+		var p = context.getBean("commentProcessor", CommentProcessor.class);
 		
 		p.setComment(c);
 		p.processComment();
 		p.validateComment();
 		c = p.getComment();
 		// 추가적 작업 수행
+		System.out.println("comment sent");
 	}
 
 }
